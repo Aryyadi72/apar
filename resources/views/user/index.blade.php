b75f572c0b21cc7f7489449240fda6902a7aea50
achmadruby <achmad.ruby@mhs.politala.ac.id>
2025-04-08 21:38:02 +0800

    080424

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -15,10 +15,12 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="card">
-                            <div class="card-header">
-                                <button class="btn btn-icon btn-primary" id="modal-1" data-toggle="modal"
-                                    data-target="#createModal"> Tambah Data</button>
-                            </div>
+                            @if (session('level') == 'Mng. HRD & GA')
+                                <div class="card-header">
+                                    <button class="btn btn-icon btn-primary" id="modal-1" data-toggle="modal"
+                                        data-target="#createModal"> Tambah Data</button>
+                                </div>
+                            @endif
                             <div class="card-body">
                                 <div class="table-responsive">
                                     <table class="table table-striped" id="table-1">
